d0ce5518c9d2b1fcf95d1fe98675823644de9961
伟 刘 <852773351@qq.com>
2020-11-29 11:31:52 +0800

    页面移动 Article移动到Article/Index

--- assignment/Pages/Article/Index.cshtml.cs
+++ assignment/Pages/Article/Index.cshtml.cs
@@ -2,21 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using assignment.Pages.Entities;
 using assignment.Pages.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace assignment.Pages
+using E = assignment.Pages.Entities;
+namespace assignment.Pages.Article
 {
-    public class ArticleModel : PageModel
+    public class IndexModel : PageModel
     {
-        public List<Article> articles { get; set; }
+        public List<E.Article> articles { get; set; }
         private ArticleRepository articleRepository { get; set; }
         public int ArticlePage { get; set; }
         public int pageSize { get; } = 2;
         public int PageIndex { get; set; }
-        public ArticleModel()
+        public IndexModel()
         {
             articleRepository = new ArticleRepository();
 
@@ -26,7 +25,7 @@ namespace assignment.Pages
             PageIndex = 1;
             if (Request.Query.ContainsKey("pageIndex"))
             {
-                 PageIndex = Convert.ToInt32(Request.Query["pageIndex"][0]);
+                PageIndex = Convert.ToInt32(Request.Query["pageIndex"][0]);
 
             }
             articles = new ArticleRepository().Get(PageIndex, pageSize);
--- assignment/Pages/Repository/ArticleRepository.cs
+++ assignment/Pages/Repository/ArticleRepository.cs
@@ -3,13 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using E = assignment.Pages.Entities;
 namespace assignment.Pages.Repository
 {
     public class ArticleRepository
     {
-        public static List<Article> articles = new List<Article>() {
-                new Article(){PublishTime=new DateTime(2020,8,20,8,7,20),
+        public static List<E.Article> articles = new List<E.Article>() {
+                new E.Article(){PublishTime=new DateTime(2020,8,20,8,7,20),
                       Id=1,
                       Title ="1-VS中的第一个页面",
                       Body = "学习HTML，其实就是学习一系列的标记（markup），而标记的核心就是元素（element）HTML标记语言的核心就是元素。 元素由标签、属性和文本内容组成，",
@@ -18,7 +18,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){ new Appraise() {Direction=UpOrdown.Up },new Appraise() {Direction=UpOrdown.Up } },
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,8,21,5,4,20),
+                new E.Article(){PublishTime=new DateTime(2020,8,21,5,4,20),
                       Id=2,
                       Title ="2-Promise：回调地狱/then()/fulfiled/reject……",
                       Body = "回调地狱在JavaScript中，异步通常就伴随着回调（复习：异步和回调）。为什么呢？看下面的代码 let result = false;function loadSuccess() {setTimeout(function () {result = true;}",
@@ -27,7 +27,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){ new Appraise() {Direction=UpOrdown.Up },new Appraise() {Direction=UpOrdown.Up },new Appraise() {Direction=UpOrdown.Up } },
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,8,5,15,20,15),
+                new E.Article(){PublishTime=new DateTime(2020,8,5,15,20,15),
                       Id=3,
                       Title ="3-JQuery：表单/Ajax/其他……",
                       Body = "表单简写: / :submit / :text ……prop()以及val()文本：text 和 textarea选择 ：check 和 radio下拉列表：select常用：取值赋值：选中只读/禁用JQuery效果hide()/show()/toggle() 淡入淡出：fade...() 滑动：slide 自定义动画：",
@@ -36,7 +36,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){ new Appraise() {Direction=UpOrdown.Up },new Appraise() {Direction=UpOrdown.Down } },
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,7,21,11,52,3),
+                new E.Article(){PublishTime=new DateTime(2020,7,21,11,52,3),
                       Id=4,
                       Title ="4-Bootstrap.js：引入/Modal/",
                       Body = "JS插件 引入bootstrap.js（基于jquery）复习：压缩.min 和 CDN第一个例子：Modaldata-js方法：modal()和option事件总结：",
@@ -45,7 +45,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){ new Appraise() {Direction=UpOrdown.Up },new Appraise() {Direction=UpOrdown.Down } },
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,1,2,1,5,2),
+                new E.Article(){PublishTime=new DateTime(2020,1,2,1,5,2),
                       Id=5,
                       Title ="5-ES6和JS进阶：新的集合对象",
                       Body = "for...in或许是为了模拟其他语言（如C#的foreach），JavaScript推出了for...in循环，可以：遍历对象的全部属性for (var i in student) {console.log(i); ",
@@ -54,7 +54,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){ new Appraise() {Direction=UpOrdown.Up },new Appraise() {Direction=UpOrdown.Down } },
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,9,1,1,5,33),
+                new E.Article(){PublishTime=new DateTime(2020,9,1,1,5,33),
                       Id=6,
                       Title ="6-Javascript的昨天今天和明天",
                       Body = "for...in或许是为了模拟其他语言（如C#的foreach），JavaScript推出了for...in循环，可以：遍历对象的全部属性for (var i in student) {console.log(i); ",
@@ -63,7 +63,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){},
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,9,9,3,52,20),
+                new E.Article(){PublishTime=new DateTime(2020,9,9,3,52,20),
                       Id=7,
                       Title ="7-JavaScript：事件：冒泡和捕获机制",
                       Body = "一般的事件处理不需要考虑这种情况。但是，这不仅是一个常见面试题，而且有其实际使用场景。演示准备 有父子两个元素<div id=\"propagate>\" <p>源栈欢迎您</p></div>为了便于演示，加上一点CSS效果：",
@@ -72,7 +72,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){ new Appraise() {Direction=UpOrdown.Up },new Appraise() {Direction=UpOrdown.Down },new Appraise() {Direction=UpOrdown.Down } },
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,7,1,23,30,30),
+                new E.Article(){PublishTime=new DateTime(2020,7,1,23,30,30),
                       Id=8,
                       Title ="8-999天晚上聊一聊：检索目录：001-111",
                       Body = "哪怕是行为艺术，我也要坚持999天！ 每天晚7点，欢迎围观 ",
@@ -81,7 +81,7 @@ namespace assignment.Pages.Repository
                       appraises = new List<Appraise>(){},
                       Author = new User(){Name ="叶飞" },
                      },
-                new Article(){PublishTime=new DateTime(2020,11,21,6,22,38),
+                new E.Article(){PublishTime=new DateTime(2020,11,21,6,22,38),
                       Id=9,
                       Title ="9-JavaScript：作用域：函数作用域/全局变量污染/命名空间",
                       Body = "在C#中我们都基本上不讲作用域，因为一切都是自然而然的（用语言描述反而有些困难）。但JavaScript的作用域，让人非常头大！局部变量如果一个变量在函数体内部申明， ",
@@ -94,7 +94,7 @@ namespace assignment.Pages.Repository
         public int ArticlesCount = articles.Count;
 
 
-        public List<Article> Get(int pageIndex,int pageSize)
+        public List<E.Article> Get(int pageIndex,int pageSize)
         {
             return articles.OrderBy(a=>a.Id).Skip((pageIndex-1)*pageSize).Take(pageSize).ToList();
         }
