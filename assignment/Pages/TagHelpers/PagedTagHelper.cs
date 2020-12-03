5b2d02bfce3753ff3f454048ed1699e8ae4ef846
伟 刘 <852773351@qq.com>
2020-12-03 10:46:14 +0800

    自定义TagHelaper

---
+++ assignment/Pages/TagHelpers/PagedTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment.Pages.TagHelpers
+{
+    [HtmlTargetElement("paged",Attributes ="path,pagedIndex")]
+    public class PagedTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            Object path = context.AllAttributes["path"].Value;
+
+            output.Attributes.RemoveAll("path");
+            output.Attributes.RemoveAll("pagedIndex");
+
+
+            object pagedIndex = context.AllAttributes["pagedIndex"].Value;
+
+            output.Attributes.Add("href",$"{path}/paged-{pagedIndex}");
+
+            base.Process(context, output);
+        }
+    }
+}
---
+++ assignment/Pages/TagHelpers/dateTimeTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment.Pages.TagHelpers
+{
+
+    [HtmlTargetElement("dateTime", Attributes = "asp-showicon,asp-only")]
+    public class dateTimeTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "small";
+            if ( context.AllAttributes["asp-showicon"].Value.ToString() == "true")
+            {
+
+            }
+            if (context.AllAttributes["asp-only"].Value.ToString() =="date")
+            {
+
+            }
+            output.PreContent.SetHtmlContent("<abc>");
+            output.PostContent.SetHtmlContent("</abc>");
+
+            //output.PreContent.SetContent("9527");
+
+
+            base.Process(context, output);
+        }
+    }
+}
