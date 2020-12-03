edc240c7e3dfd5c76a9aadfcddba8f95d39e3a4c
伟 刘 <852773351@qq.com>
2020-12-03 20:30:53 +0800

    Register页面换成TagHelper

--- assignment/Pages/Entities/User.cs
+++ assignment/Pages/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,12 @@ namespace assignment.Pages.Entities
     public class User : Entity
     {
         public string Name { get; set; }
+
+        [DataType(DataType.Password)]
         public string PassWord { get; set; }
+        public string InviterName { get; set; }
+        public int InviterCode { get; set; }
+
         public List<Article> Articles { get; set; }
         public List<Category> Categorys { get; set; }
 
--- assignment/Pages/Register.cshtml.cs
+++ assignment/Pages/Register.cshtml.cs
@@ -1,16 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using E = assignment.Pages.Entities;
 
 namespace assignment.Pages
 {
     public class RegisterModel : PageModel
     {
+        public E.User User { get; set; }
+
+        [DataType(DataType.Password)]
+        public string ConfirmPassWord { get; set; }
+        public string InputImageCode { get; set; }
         public void OnGet()
         {
+
+        }
+        public void OnPost()
+        {
+
         }
     }
 }
