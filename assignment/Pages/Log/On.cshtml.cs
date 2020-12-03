3fb1a08ac4d550680cc78d2a7c1945e32926b092
伟 刘 <852773351@qq.com>
2020-12-03 20:38:37 +0800

    Log/On页面TagHelper

--- assignment/Pages/Log/On.cshtml.cs
+++ assignment/Pages/Log/On.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using E = assignment.Pages.Entities;
 
 namespace assignment.Pages.Log
 {
     public class OnModel : PageModel
     {
+
+        public E.User User { get; set; }
+        public string InvidInputImageCode { get; set; }
         public void OnGet()
         {
         }
