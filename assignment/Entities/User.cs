ac0ca8b926541b51e4ee538cf23f6d733c067d22
伟 刘 <852773351@qq.com>
2020-12-06 18:01:46 +0800

    修改注册Bug

--- assignment/Entities/User.cs
+++ assignment/Entities/User.cs
@@ -8,22 +8,18 @@ namespace assignment.Entities
 {
     public class User : Entity
     {
-        [Required(ErrorMessage = "* 用户名不能为空")] 
+        [Required(ErrorMessage = "* 用户名不能为空")]
         public string Name { get; set; }
 
-        [DataType(DataType.Password)]
-        [Required(ErrorMessage = "* 密码不能为空")]
-        [StringLength(20, MinimumLength = 4, ErrorMessage = "* 密码的长度不能小于4，大于20")]
         public string PassWord { get; set; }
+
 
         public User InviterBy { get; set; }
 
-        [Required(ErrorMessage = "* 邀请码不能为空")]
-        [StringLength(4, MinimumLength = 4, ErrorMessage = "* 邀请码的长度只能是4位数字")]
         public string InviterCode { get; set; }
 
         public int BMony { get; set; }
-        public bool IsMale { get; set; }
+        public bool? IsMale { get; set; }
 
         public List<Message> messages { get; set; }
         public List<Article> Articles { get; set; }
--- assignment/Pages/Register.cshtml.cs
+++ assignment/Pages/Register.cshtml.cs
@@ -24,6 +24,15 @@ namespace assignment.Pages
 
         public E.User User { get; set; }
 
+        [Required(ErrorMessage = "* 邀请码不能为空")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "* 邀请码的长度只能是4位数字")]
+        public string InviterCode { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "* 密码不能为空")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "* 密码的长度不能小于4，大于20")]
+        public string PassWord { get; set; }
+
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "* 确认密码不能为空")]
         public string ConfirmPassWord { get; set; }
@@ -40,40 +49,34 @@ namespace assignment.Pages
         public void OnPost()
         {
 
-            if (User.PassWord != ConfirmPassWord)
-            {
-                ModelState.AddModelError(nameof(ConfirmPassWord), "确认密码和密码不一致");
-            }
-
-            E.User inviterBy = UserRepository.GetByName(User.InviterBy.Name);
-            if (inviterBy == null)
-            {
-                ModelState.AddModelError("User.InviterBy.Name", "邀请人不存在");
-            }
-            else if (inviterBy.InviterCode != User.InviterBy.InviterCode)
-            {
-                ModelState.AddModelError("User.InviterBy.InviterCode", "邀请码错误");
-            }
-
-
             if (!ModelState.IsValid)
             {
                 return;
             }
 
-
-            E.User InviteBy = UserRepository.GetByName(User.InviterBy.Name);
-
-            if (InviteBy == null)
+            if (PassWord != ConfirmPassWord)
             {
-
-            }
-            if (InviteBy.InviterCode != User.InviterBy.InviterCode)
-            {
-
+                ModelState.AddModelError(nameof(ConfirmPassWord), "确认密码和密码不一致");
+                return;
             }
 
-            User.InviterBy = InviteBy;
+            E.User inviterBy = UserRepository.GetByName(User.InviterBy.Name);
+            if (inviterBy == null)
+            {
+                ModelState.AddModelError("InviterBy.Name", "邀请人不存在");
+                return;
+            }
+            if (inviterBy.InviterCode != InviterCode)
+            {
+                ModelState.AddModelError(nameof(InviterCode), "邀请码错误");
+                return;
+            }
+
+
+
+
+            User.PassWord = PassWord;
+            User.InviterBy = inviterBy;
             User.Register();
             UserRepository.Save(User);
         }
