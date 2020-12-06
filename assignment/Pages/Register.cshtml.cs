66afd91075c425f6f7e13b2265fd0afd7c5aeb71
伟 刘 <852773351@qq.com>
2020-12-06 15:46:23 +0800

    登录验证

--- assignment/Pages/Register.cshtml.cs
+++ assignment/Pages/Register.cshtml.cs
@@ -21,10 +21,15 @@ namespace assignment.Pages
         {
             UserRepository = new UserRepository();
         }
+
         public E.User User { get; set; }
 
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "* 确认密码不能为空")]
         public string ConfirmPassWord { get; set; }
+
+        [Required(ErrorMessage = "* 验证码不能为空")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "* 验证码的长度只能等于4")]
         public string InputImageCode { get; set; }
 
 
@@ -34,6 +39,29 @@ namespace assignment.Pages
         }
         public void OnPost()
         {
+
+            if (User.PassWord != ConfirmPassWord)
+            {
+                ModelState.AddModelError(nameof(ConfirmPassWord), "确认密码和密码不一致");
+            }
+
+            E.User inviterBy = UserRepository.GetByName(User.InviterBy.Name);
+            if (inviterBy == null)
+            {
+                ModelState.AddModelError("User.InviterBy.Name", "邀请人不存在");
+            }
+            else if (inviterBy.InviterCode != User.InviterBy.InviterCode)
+            {
+                ModelState.AddModelError("User.InviterBy.InviterCode", "邀请码错误");
+            }
+
+
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+
+
             E.User InviteBy = UserRepository.GetByName(User.InviterBy.Name);
 
             if (InviteBy == null)
